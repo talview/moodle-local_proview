4bebf52be22e3e577d8338cd4f7594d38e314a04
Mani Ka <mani@talview.com>
2020-06-04 09:31:43 -0700

    Release 1.2.1

--- frame.php
+++ frame.php
@@ -89,11 +89,12 @@ echo $OUTPUT->header();
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script',url,'tv');
-      tv('init', authToken,{
-        session: session,
-        clear: clear || true,
-        skipHardwareTest: skipHardwareTest || false,
-        previewStyle: previewStyle || 'position: fixed; bottom: 0px;',
+        tv('init', authToken,{
+
+            session: session,
+            clear: clear || true,
+            skipHardwareTest: skipHardwareTest || false,
+            previewStyle: previewStyle || 'position: fixed; bottom: 0px;',
         initCallback: onProviewStart
       });
     };
