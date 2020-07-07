c832aba2abb285559a3f6a66e5eaafbec8197266
vidish.mishra <vidish.mishra@talview.com>
2020-07-07 13:28:36 +0530

    resolved comment

--- amd/build/proview.js
+++ amd/build/proview.js
@@ -1,7 +1,7 @@
 define(['jquery'], function($) {
   return {
    init: function(password) {
-    $(".singlebutton").find("button").click(function() {
+    $(".singlebutton.quizstartbuttondiv").find("button").click(function() {
       $(".moodle-dialogue-base").hide();
       $("#id_quizpassword").val(password);
       $("#mod_quiz_preflight_form").submit();
