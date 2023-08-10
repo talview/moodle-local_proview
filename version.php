084933e19764d71f29d8a2827f4e650b53432f7d
vaishnav-sh <vaishnav.s@talview.com>
2023-08-10 18:40:15 +0530

    version update for release

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->version  = 2023081001;
 $plugin->requires = 2020061500;
-$plugin->release = '3.0.0 (Build: 2023081001)';
+$plugin->release = '3.1.0 (Build: 2023081001)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_proview';
 
