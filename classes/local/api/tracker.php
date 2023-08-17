5f3d3362f09f241375ee9d1dbcd589d331d7ef33
Rohan Sharma <rohan.sharma@talview.com>
2023-08-17 16:24:29 +0530

    created function to fetch token

--- classes/local/api/tracker.php
+++ classes/local/api/tracker.php
@@ -37,13 +37,15 @@ defined('MOODLE_INTERNAL') || die();
  * @copyright  Talview, 2020
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class tracker {
+class tracker
+{
     /**
      * Insert the actual tracking code.
      *
      * @return void As the insertion is done through the {js} template API.
      */
-    public static function insert_tracking() {
+    public static function insert_tracking()
+    {
         global $PAGE, $OUTPUT, $USER, $DB;
 
         $pageinfo = get_context_info_array($PAGE->context->id);
@@ -54,11 +56,13 @@ class tracker {
         $template->root_dir = get_config('local_proview', 'root_dir');
         $template->profile_id = $USER->id;
 
+
         $cm = $PAGE->cm;
         if ($cm && $cm->instance) {
             $quiz = $DB->get_record('quiz', array('id' => $cm->instance));      // Fetching current quiz data for password.
             $template->quiz_password = $quiz->password;
             $template->quiz_id = $quiz->id;
+
             $attempt = $DB->get_record('quiz_attempts', array('quiz' => $quiz->id, 'userid' => $USER->id, 'state' => 'inprogress'));
             if (!$attempt) {
                 $attempts = $DB->get_records('quiz_attempts', array('quiz' => $quiz->id, 'userid' => $USER->id));
@@ -68,9 +72,18 @@ class tracker {
                 $attempt = $attempt->attempt;
             }
             $template->current_attempt = $attempt;
+            $session_id=$template->quiz_id."-".$template->profile_id."-".$template->current_attempt;
+            $attendee_id=$template->profile_id;
             if (strpos($PAGE->url, ('mod/quiz/report'))) {
-                $attempts = $DB->get_records('local_proview', array('quiz_id' => $quiz->id), 'attempt_no', 'attempt_no,proview_url');   // Fetching all the records (for proview url) for a given quiz.
+                $attempts = $DB->get_records('local_proview', array('quiz_id' => $quiz->id), 'attempt_no', 'attempt_no,proview_url');
+                foreach ($attempts as $attempt) {
+                    $token=self::generate_proview_token($template->token,$session_id,$attendee_id);
+                    $attempt->proview_url = $attempt->proview_url."?token=".$token;
+                    var_dump($attempt);
+                  var_dump(self::generate_proview_token($template->token,$session_id,$attendee_id));
+                }// Fetching all the records (for proview url) for a given quiz.
                 $template->attempts = json_encode($attempts);
+
             }
         }
 
@@ -79,5 +92,47 @@ class tracker {
             // the output; only that the $PAGE->requires are filled.
             $OUTPUT->render_from_template('local_proview/tracker', $template);
         }
+
+    }
+    public static function generate_proview_token($token,$session_id,$attendee_id) {
+        $curl = curl_init();
+//        $token="4d1ca44a-dd19-4052-b2ee-0ae600c6c688";
+//        $session_id="19-2-1";
+//        $attendee_id="189152";
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://proctoring.talview.com/v1/token/playback",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode([
+                "proctor_token" => $token,
+                "validity" => 240,
+                "external_session_id" => $session_id,
+                "external_attendee_id" => $attendee_id
+            ]),
+            CURLOPT_HTTPHEADER => [
+                "Content-Type: application/json",
+                "app-id: b37ec896-f62b-4cbe-b39f-8dd21881dfd3",
+                "is-universal-proview: true"
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+            return null;
+        } else {
+            $response_data = json_decode($response, true);
+            return $response_data["token"];
+        }
     }
 }
+
