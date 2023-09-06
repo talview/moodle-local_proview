2c712320ac35aa94277bed46757e18c20ce3943f
Rohan Sharma <rohan.sharma@talview.com>
2023-09-06 16:27:58 +0530

    merged 3.1.3

--- version.php
+++ version.php
@@ -28,9 +28,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2023090407;
+$plugin->version  = 2023090602;
 $plugin->requires = 2020061500;
-$plugin->release = '3.2.0 (Build: 2023090407)';
+$plugin->release = '3.2.0 (Build: 2023090602)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_proview';
 
